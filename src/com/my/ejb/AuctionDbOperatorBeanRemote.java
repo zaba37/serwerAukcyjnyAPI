e979791be08eec88bd33524011b610dfa7782f7f
zaba37 <zaba37gmail.com>
2015-05-19 21:35:00 +0200

    interfejs remote

---
+++ src/com/my/ejb/AuctionDbOperatorBeanRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.my.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author zaba37
+ */
+@Remote
+public interface AuctionDbOperatorBeanRemote {
+    
+}
